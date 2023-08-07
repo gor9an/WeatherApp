d7c75a951bd83efeab6b8fdea3f5351b3328ffb2
gor9an <gordanandrey@yandex.ru>
2023-08-07 16:54:11 +0300

    ended proj

--- Weather/WeatherData.swift
+++ Weather/WeatherData.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+// MARK: - WeatherData
+struct WeatherData: Codable {
+    let latitude, longitude, generationtimeMS: Double
+    let utcOffsetSeconds: Int
+    let timezone, timezoneAbbreviation: String
+    let elevation: Int
+    let currentWeather: CurrentWeather
+
+    enum CodingKeys: String, CodingKey {
+        case latitude, longitude
+        case generationtimeMS = "generationtime_ms"
+        case utcOffsetSeconds = "utc_offset_seconds"
+        case timezone
+        case timezoneAbbreviation = "timezone_abbreviation"
+        case elevation
+        case currentWeather = "current_weather"
+    }
+}
+
+// MARK: - CurrentWeather
+struct CurrentWeather: Codable {
+    let temperature, windspeed: Double
+    let winddirection, weathercode, isDay: Int
+    let time: String
+
+    enum CodingKeys: String, CodingKey {
+        case temperature, windspeed, winddirection, weathercode
+        case isDay = "is_day"
+        case time
+    }
+}
